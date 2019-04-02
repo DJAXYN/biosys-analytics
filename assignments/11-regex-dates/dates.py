3607b8e27d7a74ae28d83e2353b56cd74f67909d
Jason Giles <jasongiles@dhcp-10-142-208-193.uawifi.arizona.edu>
2019-04-01 17:13:04 -0700

    HW11

---
+++ assignments/11-regex-dates/dates.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+"""
+Author : jasongiles
+Date   : 2019-04-01
+Purpose: Rock the Casbah
+"""
+
+import os
+import sys
+import re
+
+
+# --------------------------------------------------
+def main():
+    args = sys.argv[1:]
+
+    if len(args) != 1:
+        print('Usage: {} DATE'.format(os.path.basename(sys.argv[0])))
+        sys.exit(1)
+
+    date_fh = args[0]
+
+    date_match1 = re.compile('(\d{4})'  # capture year (group 1)
+                             '[-]?'  # separator
+                             '(\d{1,2})'  # capture month (group 2)
+                             '[-]?'  # separator
+                             '(\d{1,2})?')  # capture day (group 3)
+
+    date_match2 = re.compile('(\d{1,2})'  # capture year (group 1)
+                             '[/]'  # separator
+                             '(\d{1,2})'  # capture month (group 2)
+                             '[/]?'  # separator
+                             '(\d{1,2})?')  # capture day (group 3)
+
+
+    # 3 is seperate
+
+    date_match4 = re.compile('([a-zA-Z]{1,10})'  # capture year (group 1)
+                             '[/,-]?'  # separator
+                             '\s*?'
+                             '(\d{2,4})')  # capture year (group 2))
+
+    monthsss = ("january,february,march,april,may,june,july,august,september,october,november,december")
+    months = list(monthsss.split(','))
+    #print(months)
+    months2 = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
+    #print(date_fh)
+    #for dates in date_fh:
+    dates = date_fh
+    match1 = date_match1.search(dates)
+    notany = False
+    if match1:
+        notany = True
+        checkday = match1.group(3)
+        if checkday !=None and len(checkday)<2:
+            checkday = str(0)+str(checkday)
+        formater1= '{}-{}-{}'.format(match1.group(1),match1.group(2) if len(match1.group(2))>1 else str(0)+match1.group(2),'01' if match1.group(3)==None else checkday)
+        print(formater1)
+    match2 = date_match2.match(dates)
+
+    if match2:
+        notany = True
+        if match2.group(3)!=None and len(match2.group(3))<2:
+            redo = str(0)+str(match.group(3))
+            formater2= '{}-{}-{}'.format(match2.group(2)if len(match2.group(2))==4 else str(20)+match2.group(2), match2.group(1)if len(match2.group(1))==2 else str(0)+match2.group(1),'01' if match2.group(3)==None else redo)
+            print(formater2)
+        else:
+            formater2 = '{}-{}-{}'.format(match2.group(2) if len(match2.group(2)) == 4 else str(20) + match2.group(2),
+                                          match2.group(1) if len(match2.group(1)) == 2 else str(0) + match2.group(1),
+                                          '01' if match2.group(3) == None else match2.group(3))
+            print(formater2)
+
+    match4 = date_match4.search(dates)
+    if match4:
+        notany = True
+        #print(match4)
+        numb = match4.group(1).lower()
+        #print(numb[:3])
+        if numb[:3] in months2:
+            monthday = months2.index(numb[:3]) + 1
+            formater4= '{}-{}-{}'.format(match4.group(2),str(0)+str(monthday) if monthday<10 else monthday,'01')
+            print(formater4)
+    if not notany:
+        print('No match')
+
+# --------------------------------------------------
+main()
