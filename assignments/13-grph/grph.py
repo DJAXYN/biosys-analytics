6d171a56a267ed2ac59aecf4a518a4502322e9d1
Jason B. Giles <djaxyn@pm.me>
2019-04-18 09:22:08 -0700

    HW13 grad

--- assignments/13-grph/grph.py
+++ assignments/13-grph/grph.py
@@ -47,6 +47,39 @@ def die(msg='Something bad happened'):
     sys.exit(1)
 
 
+
+def test_find_kmers():
+    """Test the `find_kmers` function"""
+    assert grph.find_kmers('ACTG', 2) == ['AC', 'CT', 'TG']
+    assert grph.find_kmers('ACTG', 3) == ['ACT', 'CTG']
+    assert grph.find_kmers('ACTG', 4) == ['ACTG']
+
+
+
+# --------------------------------------------------
+
+def find_kmers(string,k_mer):
+    args = sys.argv[1:]
+    num_args = len(args)
+    if not 1 <= num_args <= 2:
+        print('Usage: {} STR [K]'.format(os.path.basename(sys.argv[0])))
+        sys.exit(1)
+
+    string = args[0]
+    k_mer= args[1] if num_args == 2 else '3'
+    if not k_mer.isdigit():
+        print('k "{}" is not a digit'.format(k_mer))
+        sys.exit(1)
+    k = int(k_mer)
+    if len(string) < k:
+        print('There are no {}-length substrings in "{}"'.format(k, string))
+    else:
+        n = len(string) - k + 1
+        for i in range(0, n):
+            print(string[i:i + k])
+
+
+
 # --------------------------------------------------
 def main():
     """Make a jazz noise here"""
@@ -54,8 +87,31 @@ def main():
     k_mer = args.overlap
     fasta_filez = args.fasta_file
 
-    if len(k_mer)<1:
-        print('')
+    if k_mer<1:
+        die('-k "{}" must be a positive integer'.format(k_mer))
+
+    if not os.path.isfile(fasta_filez):
+        die('"{}" is not a file'.format(fasta_filez))
+
+    base = dict()
+
+    for record in SeqIO.parse(fasta_filez,"fasta"):
+        #print(record.id)
+        base[record.id] = record.seq
+
+    for i in base.keys():
+        for j in base.keys():
+            seq1 = base.get(i)
+            seq2 = base.get(j)
+            #print(seq1,'hello')
+            #print(seq1[-k_mer:])
+            #print(seq1[:k_mer])
+            back_seq1 = (seq1[-k_mer:])
+            front_seq2 = (seq2[:k_mer])
+            if j!=i and back_seq1==front_seq2:
+                print('{} {}'.format(i,j))
+
+
 
 
 # --------------------------------------------------
